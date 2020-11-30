42f6b6ead3f74ee053ba704996744f22cb1e3bd2
Poisson2Baleine <1848611319@qq.com>
2020-12-01 00:45:52 +0800

    fix export problem

--- error.go
+++ error.go
@@ -3,43 +3,43 @@ package errors
 import "errors"
 
 var (
-	serverError           = errors.New("服务器内部错误")
-	paramError            = errors.New("参数错误")
-	frequentlyError       = errors.New("访问太过频繁")
-	busyError             = errors.New("服务器繁忙")
-	permissionDeniedError = errors.New("没有权限")
-	notFoundError         = errors.New("资源不存在")
-	timeOutError          = errors.New("请求超时")
-	rpcCallError          = errors.New("RPC调用失败")
-	redisConnError        = errors.New("REDIS连接失败")
-	mysqlConnError        = errors.New("MYSQL连接失败")
-	etcdConnError         = errors.New("ETCD连接失败")
-	unknownError          = errors.New("未知错误")
+	ServerError           = errors.New("服务器内部错误")
+	ParamError            = errors.New("参数错误")
+	FrequentlyError       = errors.New("访问太过频繁")
+	BusyError             = errors.New("服务器繁忙")
+	PermissionDeniedError = errors.New("没有权限")
+	NotFoundError         = errors.New("资源不存在")
+	TimeOutError          = errors.New("请求超时")
+	RpcCallError          = errors.New("RPC调用失败")
+	RedisConnError        = errors.New("REDIS连接失败")
+	MysqlConnError        = errors.New("MYSQL连接失败")
+	EtcdConnError         = errors.New("ETCD连接失败")
+	UnknownError          = errors.New("未知错误")
 )
 
 func GetErrorCode(err error) int {
 	switch err {
-	case serverError:
+	case ServerError:
 		return 50000
-	case paramError:
+	case ParamError:
 		return 40000
-	case frequentlyError:
+	case FrequentlyError:
 		return 40010
-	case busyError:
+	case BusyError:
 		return 50010
-	case permissionDeniedError:
+	case PermissionDeniedError:
 		return 40001
-	case notFoundError:
+	case NotFoundError:
 		return 40004
-	case timeOutError:
+	case TimeOutError:
 		return 50002
-	case rpcCallError:
+	case RpcCallError:
 		return 50003
-	case redisConnError:
+	case RedisConnError:
 		return 56379
-	case mysqlConnError:
+	case MysqlConnError:
 		return 53306
-	case etcdConnError:
+	case EtcdConnError:
 		return 52379
 	default:
 		return 55555
